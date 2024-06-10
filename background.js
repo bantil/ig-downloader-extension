eadbe4543784cf1ab08cfa792e076331233ff4bf
Brandon A <bantillon@pm.me>
2024-06-09 22:25:38 -0700

    LMAO

--- background.js
+++
@@ -1,9 +0,0 @@
-browser.webNavigation.onCompleted.addListener((details) => {
-    // Ensure the frameId is 0, meaning it's the main frame, not an iframe
-    if (details.frameId === 0) {
-      // Inject the content script
-      browser.tabs.executeScript(details.tabId, {
-        file: 'main.js'
-      });
-    }
-  }, {url: [{hostContains: "www.instagram.com"}]});
